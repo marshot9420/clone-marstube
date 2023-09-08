26c4cc82f137a251fc5cb2b68659ecd260ca956e
marshot <124557537+marshot9420@users.noreply.github.com>
2023-09-08 10:18:19 +0900

    Chore: 환경변수 설정

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+import dotenv from 'dotenv';
+import path from 'path';
+import express from 'express';
+
+const envPath = path.join(__dirname, `../.env.${process.env.NODE_ENV}`);
+dotenv.config({ path: envPath });
+
+const app = express();
+
+const BASE_URL = process.env.BASE_URL;
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () => {
+  console.log(`✅ Server listening on: ${BASE_URL}:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
