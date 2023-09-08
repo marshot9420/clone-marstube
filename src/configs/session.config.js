d33f305cc85ba3e30d8069da555fcab230ddee93
marshot <124557537+marshot9420@users.noreply.github.com>
2023-09-08 19:27:44 +0900

    Feat: 헤더 설정 추가

--- src/configs/session.config.js
+++ src/configs/session.config.js
@@ -12,4 +12,13 @@ export const sessionConfig = (app) => {
       }),
     }),
   );
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+  });
 };
